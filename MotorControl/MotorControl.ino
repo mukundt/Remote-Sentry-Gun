e42eee34e01f55c62d9eae9ebe2fcb259043de71
Mukund Tibrewala <mukund.tibrewala@gmail.com>
2014-02-13 22:38:08 -0500

    Added RL_Stepper.h - stepper motor control interface

---
+++ MotorControl/MotorControl.ino
@@ -0,0 +1,23 @@
+//
+//  MotorControl.ino
+//  Remote-Sentry-Gun
+//
+//  Created by Mukund Tibrewala on 13/2/14.
+//  www.mukundt.com
+//  Copyright (c) 2014 Project Sentry Gun. All rights reserved.
+//
+
+#include <SPI.h>
+#include <boards.h>
+#include <ble_shield.h>
+#include <services.h> 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ RL_Stepper.h
@@ -0,0 +1,55 @@
+/*
+  RL_Stepper.h - Library for controlling stepper motors.
+  Version 1.0
+  Created by Bob Rudolph, February 13, 2014.
+  Rudolph Labs:  http://www.rudolphlabs.com/
+*/
+
+// ensure this library description is only included once
+#ifndef RL_Stepper_h
+#define RL_Stepper_h
+
+#include "Arduino.h"
+
+// library interface description
+class Stepper {
+
+  public:
+    // constructors:
+    Stepper(int step_pin, int enable_pin, int direction_pin);
+
+    // methods to override default values:
+    void set_delay(float delay_milliseconds);
+    void set_dwell(int dwell_microseconds);
+    void set_count(int new_count);
+    void invert_direction(boolean yes_no);
+
+    // methods to move motor:
+    void move_relative(int num_steps);
+    void move_absolute(int step_number);
+
+    // methods to enable/disable motor:
+    void off();
+    void on();
+
+    int get_count();
+    boolean is_on();
+
+  private:
+    // pin numbers:
+    int _step_pin;
+    int _enable_pin;
+    int _direction_pin;
+
+    // delay parameters:
+    int _delay;
+    boolean _delay_in_microseconds;
+    int _dwell_microseconds;
+
+    boolean _invert_direction;
+    int _count;
+    boolean _is_on;
+
+};
+
+#endif
